5bc06361e316124053a2d49edc8382b2cf4694e4
arolave <andres@thinkingbytes.net>
2020-01-13 19:07:42 +0200

    lint ifx

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ module.exports = {
   rules,
   configs: {
     recommended: {
-      rules: { ...internalRecommendedRules, ...externalRecommendedRules }
+      rules: {...internalRecommendedRules, ...externalRecommendedRules}
     }
   }
 };
