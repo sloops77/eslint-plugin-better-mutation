aedc5ee68ec3b05e2c63fda44a06f4ae5ed08be1
arolave <andres@thinkingbytes.net>
2020-01-13 19:06:03 +0200

    fix lint

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ module.exports = {
   rules,
   configs: {
     recommended: {
-      rules: Object.assign({}, internalRecommendedRules, externalRecommendedRules)
+      rules: { ...internalRecommendedRules, ...externalRecommendedRules }
     }
   }
 };
