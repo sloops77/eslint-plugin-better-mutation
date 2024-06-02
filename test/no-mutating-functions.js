e2b32af836933670b5ee39e9a19f579bd34a9a5e
sloops77 <andres.olave@thinkingbytes.net>
2024-06-02 19:54:04 +1000

    chore: upgrade ava dependency

--- test/no-mutating-functions.js
+++ test/no-mutating-functions.js
@@ -1,6 +1,6 @@
-import test from 'ava';
-import avaRuleTester from 'eslint-ava-rule-tester';
-import rule from '../rules/no-mutating-functions';
+const test = require('ava');
+const avaRuleTester = require('eslint-ava-rule-tester');
+const rule = require('../rules/no-mutating-functions');
 
 const ruleTester = avaRuleTester(test, {
   env: {
