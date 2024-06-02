f78c813923465c3c62adafdfd498fed468c17d50
Andres Olave <sloops77@users.noreply.github.com>
2024-06-03 07:00:47 +1000

    chore: xo upgrade (#60)
    * chore: yarn.lock
    
    * chore: upgrade xo
    
    ---------
    
    Co-authored-by: sloops77 <andres.olave@thinkingbytes.net>

--- test/no-mutating-methods.js
+++ test/no-mutating-methods.js
@@ -4,15 +4,15 @@ const rule = require('../rules/no-mutating-methods');
 
 const ruleTester = avaRuleTester(test, {
   env: {
-    es6: true
+    es6: true,
   },
   parserOptions: {
-    sourceType: 'module'
-  }
+    sourceType: 'module',
+  },
 });
 
 const methodError = methodName => ({
-  message: `The use of method \`${methodName}\` is not allowed as it might be a mutating method`
+  message: `The use of method \`${methodName}\` is not allowed as it might be a mutating method`,
 });
 
 ruleTester.run('no-mutating-methods', rule, {
@@ -26,141 +26,141 @@ ruleTester.run('no-mutating-methods', rule, {
     {
       code: '_.push(a, b)',
       options: [{
-        allowedObjects: ['_']
-      }]
+        allowedObjects: ['_'],
+      }],
     },
     {
       code: '_.sort(a, b)',
       options: [{
-        allowedObjects: ['_']
-      }]
+        allowedObjects: ['_'],
+      }],
     },
     {
       code: 'R.reverse(a, b)',
       options: [{
-        allowedObjects: ['R']
-      }]
+        allowedObjects: ['R'],
+      }],
     },
     {
       code: 'R.sort(a, b)',
       options: [{
-        allowedObjects: ['R']
-      }]
+        allowedObjects: ['R'],
+      }],
     },
     'Object.keys(a)',
     'Object.values(a)',
     'var value = []; value.copyWithin(a);',
     'let array = [1,2,3]; reduce((acc, x) => { acc.push(1); return acc; }, array);',
     'let array = [1,2,3]; array.reduce((acc, x) => { acc.push(1); return acc; });',
-    'let array = [1,2,3]; array.reduce((acc, x) => { if (x==1) { acc.push(1); } return acc; });'
+    'let array = [1,2,3]; array.reduce((acc, x) => { if (x==1) { acc.push(1); } return acc; });',
   ],
   invalid: [
     {
       code: 'value.copyWithin(a);',
-      errors: [methodError('copyWithin')]
+      errors: [methodError('copyWithin')],
     },
     {
       code: 'var value; value.copyWithin(a);',
-      errors: [methodError('copyWithin')]
+      errors: [methodError('copyWithin')],
     },
     {
       code: 'value.pop(a);',
-      errors: [methodError('pop')]
+      errors: [methodError('pop')],
     },
     {
       code: 'value.push(a);',
-      errors: [methodError('push')]
+      errors: [methodError('push')],
     },
     {
       code: 'value.reverse(a);',
-      errors: [methodError('reverse')]
+      errors: [methodError('reverse')],
     },
     {
       code: 'value.shift(a);',
-      errors: [methodError('shift')]
+      errors: [methodError('shift')],
     },
     {
       code: 'value.sort(a);',
-      errors: [methodError('sort')]
+      errors: [methodError('sort')],
     },
     {
       code: 'value.splice(a);',
-      errors: [methodError('splice')]
+      errors: [methodError('splice')],
     },
     {
       code: 'value.unshift(a);',
-      errors: [methodError('unshift')]
+      errors: [methodError('unshift')],
     },
     {
       code: 'value.watch(a);',
-      errors: [methodError('watch')]
+      errors: [methodError('watch')],
     },
     {
       code: 'value.unwatch(a);',
-      errors: [methodError('unwatch')]
+      errors: [methodError('unwatch')],
     },
     {
       code: '_.sort(a)',
-      errors: [methodError('sort')]
+      errors: [methodError('sort')],
     },
     {
       code: 'value["push"](a)',
-      errors: [methodError('push')]
+      errors: [methodError('push')],
     },
     {
       code: 'value["pop"](a)',
-      errors: [methodError('pop')]
+      errors: [methodError('pop')],
     },
     {
       code: 'value.push(a)',
       options: [{
-        allowedObjects: ['foo']
+        allowedObjects: ['foo'],
       }],
-      errors: [methodError('push')]
+      errors: [methodError('push')],
     },
     {
       code: 'R.sort(a)',
       options: [{
-        allowedObjects: ['_']
+        allowedObjects: ['_'],
       }],
-      errors: [methodError('sort')]
+      errors: [methodError('sort')],
     },
     {
       code: 'R.sort(a)',
       options: [{
-        allowedObjects: ['ramda']
+        allowedObjects: ['ramda'],
       }],
-      errors: [methodError('sort')]
+      errors: [methodError('sort')],
     },
     {
       code: 'R.foo().sort(a)',
       options: [{
-        allowedObjects: ['R']
+        allowedObjects: ['R'],
       }],
-      errors: [methodError('sort')]
+      errors: [methodError('sort')],
     },
     {
       code: 'R.foo.sort(a)',
       options: [{
-        allowedObjects: ['R']
+        allowedObjects: ['R'],
       }],
-      errors: [methodError('sort')]
+      errors: [methodError('sort')],
     },
     {
       code: 'foo().sort(a)',
-      errors: [methodError('sort')]
+      errors: [methodError('sort')],
     },
     {
       code: 'R().sort(a)',
       options: [{
-        allowedObjects: ['R']
+        allowedObjects: ['R'],
       }],
-      errors: [methodError('sort')]
+      errors: [methodError('sort')],
     },
     {
       code: 'let array = [1,2,3]; array.reduce((acc, x) => { acc.push(1); return acc; });',
       options: [{reducers: []}],
-      errors: [methodError('push')]
-    }
-  ]
+      errors: [methodError('push')],
+    },
+  ],
 });
