61d9e25a07f7c764b20b26f2e7ee53995f80666a
Andres Olave <arolave@gmail.com>
2025-04-24 17:56:59 +1000

    feat: support flat config

--- index.js
+++ index.js
@@ -21,13 +21,26 @@ const plugin = {
     name: packageJson.name,
     version: packageJson.version,
   },
+  configs: {},
   rules,
-  configs: {
-    recommended: {
-      rules: {...internalRecommendedRules, ...externalRecommendedRules},
-    },
-  },
+  processors: {},
   defaults,
 };
+
+Object.assign(plugin.configs, {
+  'flat/recommended': [{
+    plugins: {
+      'better-mutation': plugin,
+    },
+    rules: {...internalRecommendedRules, ...externalRecommendedRules},
+  }],
+  recommended: {
+    root: false, // Hack to get this configuration seen as legacy plugin
+    plugins: ['better-mutation'],
+    rules: {...internalRecommendedRules, ...externalRecommendedRules},
+  },
+});
+
+module.exports.default = plugin;
 
 module.exports = plugin;
