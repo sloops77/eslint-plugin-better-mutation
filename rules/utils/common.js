6b29056adf8222143e9943024ab44ff4f8159897
Piotr Ładoński <piolado@gmail.com>
2025-05-08 11:40:30 +0200

    fix: handle undefined exemptedInitializers

--- rules/utils/common.js
+++ rules/utils/common.js
@@ -65,7 +65,7 @@ const isExemptedInitializer = (rhsExpression, exemptedInitializers) => {
   }
 
   const initializer = buildInitializer(rhsExpression.callee);
-  if (exemptedInitializers.includes(initializer)) {
+  if (exemptedInitializers?.includes(initializer)) {
     return true;
   }
 
